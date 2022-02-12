241825bf316eecb049775c2897c467618d0110c7
Manav Kaushal <manavkaushal756@gmail.com>
2022-02-12 16:52:43 +0530

    header and footer added to Layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,7 @@ export const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      {children}
+      <div className="px-4 sm:px-6 lg:px-8 py-4">{children}</div>
       <Footer />
     </div>
   );
