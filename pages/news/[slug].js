dc99cfc7b5f65f4c49c87d2f6709de7003b92996
Manav Kaushal <manavkaushal756@gmail.com>
2022-02-14 04:08:20 +0530

    added env file, WIP backend

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -9,7 +9,7 @@ const SingleNews = ({ news }) => {
   const slug = router.query.slug;
 
   return (
-    <Container title={`${slug} | ${company.name}`}>
+    <Container title={`${news.name} | ${company.name}`}>
       <div className="py-16 xl:py-36 px-4 sm:px-6 lg:px-8 bg-white overflow-hidden">
         <div className="max-w-max lg:max-w-7xl mx-auto">
           <div className="relative z-10 mb-8 md:mb-2 md:px-6">
